ade0c85cb1d4a938c79a60e29b65b7bb6b6d1ee9
ViraVarguliak <varvera1486@gmail.com>
2025-04-17 15:01:14 +0200

    Lesson 3

--- homework/homework02/hom2/src/Main.java
+++ homework/homework02/hom2/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Thread task2 = new Task(2,2);
+        Thread task3 = new Task(3,3);
+
+        task2.setDaemon(true);
+        task3.setDaemon(true);
+
+        task2.start();
+        task3.start();
+
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("--- game over ----");
+
     }
 }
---
+++ homework/homework02/hom2/src/Task.java
@@ -0,0 +1,25 @@
+public class Task extends Thread {
+
+    private final int factor;
+    private final int start;
+
+    public Task(int factor, int start) {
+        this.factor = factor;
+        this.start = start;
+
+    }
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        for (int i = start; true; i += factor) {
+            System.out.println(name + " на " + factor + ": " + i);
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+}
